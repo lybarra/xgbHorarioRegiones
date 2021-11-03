9a28d0a8c1010a5e18e10a3ce308fd3a5bf36b47
lybarra <63879442+lybarra@users.noreply.github.com>
2021-11-03 12:48:52 -0300

    avance 3/11, ya corre automaticamente

--- pronDemaHoraria.py
+++ pronDemaHoraria.py
@@ -9,12 +9,30 @@ import pandas as pd
 import numpy as np
 from datetime import timedelta,date
 from xgboost.sklearn import XGBRegressor
-import os
+from configparser import ConfigParser
 
-from sklearn.preprocessing import OneHotEncoder
+import os
+# from sklearn.preprocessing import OneHotEncoder
 import joblib
 
-script_dir = os.path.dirname(__file__)
+# script_dir = os.path.dirname(__file__)
+
+#leo archivos de configuracion
+config = ConfigParser()
+privConfig = ConfigParser()
+privConfig.read('private.cfg')
+config.read('config.cfg')
+
+pathFeriados = privConfig.get('PATHS','pathFeriados')
+pathPronosticos = privConfig.get('PATHS', 'pathPronosticos')
+pathCovid = privConfig.get('PATHS', 'pathCovid')
+pathSalida = privConfig.get('PATHS', 'pathSalida')
+
+
+featMeteoPron = config.get('FEATURES','featMeteoPron').split(',')
+featCat = config.get('FEATURES','featCat').split(',')
+featFeriado = config.get('FEATURES','featFeriado').split(',')
+regionesElectricas = config.get('REGIONES','regionesElectricas').split(',')
 
 
 ##Datos del dia
@@ -23,201 +41,79 @@ dia=hoy.strftime('%d')
 mes=hoy.strftime('%m')
 anio=hoy.strftime('%y')
 
-tmin=['RosarioMin','CordobaMin','BahiaBlancaMin',
-          'TucumanMin','ResistenciaMin','NeuquenMin','MendozaMin','CapitalMin']
-tmax=['RosarioMax','CordobaMax','BahiaBlancaMax',
-          'TucumanMax','ResistenciaMax','NeuquenMax','MendozaMax','CapitalMax']
-
 
 ################# INPUTS PARA PRONOSTICAR ##################
-in_path=r'\\vranac\ProSemDi\PRONOSTICO DEMANDA\INPUT'
-pron2=dia+mes+anio+'.xlsx'
-path_pron=os.path.join(in_path,pron2)
-data_pron=pd.read_excel(path_pron,sheet_name='TEMP',index_col=0,parse_dates=True)
-data_pron=data_pron.asfreq('D')
-data_pron=data_pron.astype('float')
 
-data_dem=pd.read_excel(path_pron,sheet_name='DEMA',index_col=0,parse_dates=True)
-data_nub=pd.read_excel(path_pron,sheet_name='NUBOSIDAD',index_col=0,parse_dates=True)
-data_nub.replace(nub_dic,inplace=True)
+#importo pronosticos
+dfPronosticos = pd.read_excel(pathPronosticos,index_col=0, parse_dates=True)
+#variables categoricas calendario
+dfPronosticos['TIPODIA'] = dfPronosticos.index.dayofweek
+dfPronosticos['MES'] = dfPronosticos.index.month
+dfPronosticos.rename(columns={'Hora':'HORA','Dia':'DIA'},inplace=True) #para que quede acorde
 
+# Importo Feriados
+dfFer = pd.read_excel(pathFeriados,index_col=0)
+dfFer.drop(['Descripcion','Tipo'],inplace=True,axis=1) 
+dfFer.set_index(pd.to_datetime(dfFer.index),inplace=True) # sin esto el join arroja error
+dfFer.index.names = ['DIA'] #renombro para el join
 
-fer='FERIADOS.xlsx'
-path_fer=os.path.join(in_path,fer)
-data_fer=pd.read_excel(path_fer,index_col=0,parse_dates=True)
-data_fer.drop(labels=['Descripcion'],axis=1,inplace=True)
-data_fer=data_fer.asfreq('D')
-data_fer['FeriadoManiana']=data_fer['Feriado'].shift(-1)
-data_fer['FeriadoAyer']=data_fer['Feriado'].shift(1)
-data_fer.fillna(0,inplace=True)
+dfFer = dfFer.asfreq('D',fill_value=0) #completo todo el calendario
 
-#Otros Inputs
-covid='COVID.xlsx'
-path_covid=os.path.join(in_path,covid)
-data_covid=pd.read_excel(path_covid,index_col=0,parse_dates=True)
-#----------------------------------------------------------
+dfFer['L.Feriado'] = dfFer['Feriado'].shift(1)
+dfFer['F.Feriado'] = dfFer['Feriado'].shift(-1)
+dfFer['F.Feriado'].fillna(0,inplace=True) #el día posterior al último dato no se considera feriado
+dfFer['L.Feriado'].fillna(0,inplace=True) #el día anteriro al primer dato no se considera feriado
 
-#%%
-x_for=pd.merge(data_pron,data_nub,left_index=True,right_index=True,how='left')
-x_for=pd.merge(x_for,data_dem,left_index=True,right_index=True,how='left')
-x_for=pd.merge(x_for,data_covid,left_index=True,right_index=True,how='left')
-x_for=pd.merge(x_for,data_fer,left_index=True,right_index=True,how='left')
+# join con el df de feriados
+dfPronosticos = dfPronosticos.join(dfFer,on='DIA')
 
-x_for['Feriado'].fillna(0,inplace=True)
-x_for['FeriadoAyer'].fillna(0,inplace=True)
-x_for['FeriadoManiana'].fillna(0,inplace=True)
+# modelado del efecto de la cuarentena
+dfCovid = pd.read_excel(pathCovid, index_col = 0, parse_dates = True)
+dfCovid = dfCovid.asfreq('D')
+dfCovid.index.names = ['DIA']
 
-#%%
-#Genero las columnas para rellenar
-x_for['MEM']=np.zeros(x_for.shape[0])
-x_for['MEM Pico']=np.zeros(x_for.shape[0])
-x_for['MEMmin']=np.zeros(x_for.shape[0])
+dfPronosticos = dfPronosticos.join(dfCovid, on = 'DIA')
+dfPronosticos['COVID'].fillna(0,inplace = True)
+#
 
-#Genero las variables calendario
-x_for['Fecha']=pd.to_datetime(x_for.index)
-x_for['Mes'] = x_for['Fecha'].dt.month
-x_for['Dia'] = x_for['Fecha'].dt.dayofweek
-#Reemplazo los mie,jue,vie (2, 3 y 4) por martes (1)
-# x_for['Dia'].replace(to_replace=[2,3,4], value=1,inplace=True)
+# defino las features de temperatura al cuadrado y cubo 
+tempCols = [col for col in dfPronosticos.columns if 'T2m' in col]
 
+for col in tempCols:
+    dfPronosticos[col+'2'] = dfPronosticos[col]**2
+    dfPronosticos[col+'3'] = dfPronosticos[col]**3
 
-tmax2, tmin2, tmax3, tmin3 =[],[],[],[]
+# uso pronosticos de PBS para BAS
+dfPronosticos.columns = dfPronosticos.columns.str.replace('PBS','BAS')
 
-for i in tmin:
-    x_for[i+'2']=x_for[i]**2
-    tmin2.append(i+'2')
-    x_for[i+'3']=x_for[i]**3
-    tmin3.append(i+'3')
-    
-for i in tmax:
-    x_for[i+'2']=x_for[i]**2
-    tmax2.append(i+'2')
-    x_for[i+'3']=x_for[i]**3
-    tmax3.append(i+'3')
-
-#%%
-## CARGO EL ONE HOT ENCODER
-cat=['Feriado','FeriadoAyer','FeriadoManiana']
-cat2=['Dia','NUBOSIDAD','Mes'] 
-
-
-enc_path = os.path.join(script_dir, 'encoder.joblib')
-
-enc = joblib.load(enc_path)
-
-ohe_cat = enc.get_feature_names(cat2)
-
-enc_df = pd.DataFrame(data = enc.transform(x_for[cat2]).toarray(),
-                           columns = ohe_cat,
-                           index = x_for.index,
-                           dtype = bool)
-
-x_for=x_for.join(enc_df)
-
-#Defino el DataFrame para
-x_for_rl=pd.DataFrame(data=x_for.values,index=x_for.index,columns=x_for.columns)
-x_for_rl.fillna(0)
-
-#%%
-
-# FEATURES PARA ENTRENAR
-misc=['COVID']
-feats = tmax + tmin + misc + cat + ohe_cat.tolist() + tmax2 + tmin2 + tmax3 + tmin3
-# Genero variables para que sea mas facil aniadirlas a una lista
-mem_ene=['L.MEM']
-mem_pico=['L.MEMPico']
-mem_min=['L.MEMmin']
 
 
 #%%
 
 # MODELOS XGBOOST 
-xgb_mem=XGBRegressor()
-xgb_pico=XGBRegressor()
-xgb_min=XGBRegressor()
+xgbModel=XGBRegressor()
+modeloPron = 'GFS'
 
+for region in regionesElectricas: 
 
-xgb_mem_path = os.path.join(script_dir, 'xgb_mem.json')
-xgb_pico_path = os.path.join(script_dir, 'xgb_pico.json')
-xgb_min_path = os.path.join(script_dir, 'xgb_min.json')
+    xgbModel.load_model(r'json\{}_xgb.json'.format(region))
 
-xgb_mem.load_model(xgb_mem_path)
-xgb_pico.load_model(xgb_pico_path)
-xgb_min.load_model(xgb_min_path)
-
-# MODELOS REG LINEAL
-
-# rl_pico=LinearRegression()
-# rl_mem=LinearRegression()
-# rl_min=LinearRegression()
-
-pico_path = os.path.join(script_dir, 'rl_pico.joblib')
-rl_pico=joblib.load(pico_path)
-
-mem_path = os.path.join(script_dir, 'rl_mem.joblib')
-rl_mem=joblib.load(mem_path)
-
-min_path = os.path.join(script_dir, 'rl_min.joblib')
-rl_min=joblib.load(min_path)
-
+    featAux = ['{}_{}_{}'.format(region,modeloPron,i) for i in featMeteoPron]
+    #Lista de features, la union de todas
+    feats = featAux + featCat + featFeriado
+    
+    dfPronosticos['pron{}.xgb'.format(region)] = xgbModel.predict(dfPronosticos[feats],validate_features=False).round(0)    
 
 
 
 #%%
+# archivo de salida
 
+outList = [col for col in dfPronosticos.columns if 'pron' in col]
 
-for idx in range(x_for.shape[0]):
-    mem_iter=xgb_mem.predict(x_for[feats+mem_pico].iloc[[idx]],validate_features=False)
-    x_for['MEM'].iloc[idx]=mem_iter
-    
-    pico_iter=xgb_pico.predict(x_for[feats+mem_pico].iloc[[idx]],validate_features=False)
-    x_for['MEM Pico'].iloc[idx]=pico_iter
-    
-    min_iter=xgb_min.predict(x_for[feats+mem_pico].iloc[[idx]],validate_features=False)
-    x_for['MEMmin'].iloc[idx]=min_iter
-    
-    if idx+1 in range(x_for.shape[0]):
-        x_for['L.MEM'].iloc[idx+1]=mem_iter
-        x_for['L.MEMPico'].iloc[idx+1]=pico_iter    
-        x_for['L.MEMmin'].iloc[idx+1]=min_iter  
-        
-        
-#%%
+wr = pd.ExcelWriter(r'{}{}{}{}_reg.xlsx'.format(pathSalida,dia,mes,anio))#,engine='xlsxwriter'
 
-
-
-for idx in range(x_for_rl.shape[0]):
-    mem_iter=rl_mem.predict(x_for_rl[feats+mem_pico].iloc[[idx]])[0]
-    x_for_rl['MEM'].iloc[idx]=mem_iter
-    
-    pico_iter=rl_pico.predict(x_for_rl[feats+mem_pico].iloc[[idx]])[0]
-    x_for_rl['MEM Pico'].iloc[idx]=pico_iter
-    
-    min_iter=rl_min.predict(x_for_rl[feats+mem_pico].iloc[[idx]])[0]
-    x_for_rl['MEMmin'].iloc[idx]=min_iter
-    
-    if idx+1 in range(x_for_rl.shape[0]):
-        x_for_rl['L.MEM'].iloc[idx+1]=mem_iter
-        x_for_rl['L.MEMPico'].iloc[idx+1]=pico_iter    
-        x_for_rl['L.MEMmin'].iloc[idx+1]=min_iter  
-
-
-
-#%%
-out=pd.DataFrame(x_for.loc[:,['MEM','MEM Pico','MEMmin']],index=x_for.index)
-
-out['MEM_rl']=x_for_rl['MEM']
-out['MEM Pico_rl']=x_for_rl['MEM Pico']
-out['MEMmin_rl']=x_for_rl['MEMmin']
-
-
-#Escritura del archivo
-path1 = r'\\vranac\ProSemDi\PRONOSTICO DEMANDA\OUTPUT'
-path2 = dia +mes + anio + '.xlsx'
-
-path=os.path.join(path1,path2)
-wr=pd.ExcelWriter(path)#,engine='xlsxwriter'
-out.to_excel(wr,sheet_name='DEMANDA')                  
+dfPronosticos.loc[:,outList].to_excel(wr,sheet_name='DEMANDA')                  
 
 wr.save()
 wr.close()
--- train_modHH.py
+++ train_modHH.py
@@ -6,19 +6,16 @@ Created on Tue Jun 15 10:30:02 2021
 """
 import pyodbc
 import pandas as pd
+import numpy as np
 import glob
 from configparser import ConfigParser
 from ast import literal_eval #para leer dict de cfg
 from datetime import date, timedelta
-from scipy.signal import savgol_filter #filtro suavisador
+from scipy.signal import savgol_filter #filtro suavisador media movil
 
 from xgboost.sklearn import XGBRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
 
-# Si quisiera plotear
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# sns.set(rc={'figure.figsize':(17, 8)})
 
 #Hay dos archivos de cfg, uno para informacion privada y otro para datos generales
 #instancio y leo los archivos de conf
@@ -37,22 +34,28 @@ regionesEstaciones = literal_eval(config.get('REGIONES','regionesEstaciones'))
 regionesElectricas = config.get('REGIONES','regionesElectricas').split(',')
 acerias = literal_eval(config.get('ACERIAS','acerias'))
 
+featMeteo = config.get('FEATURES','featMeteo').split(',')
+featCat = config.get('FEATURES','featCat').split(',')
+featFeriado = config.get('FEATURES','featFeriado').split(',')
 
-varMeteo=['TEMP','HUM','PNM','DD','FF']
+
+#Lista de features, la union de todas
+feats = featMeteo + featCat + featFeriado 
 
 
-#instancio la conexion a la BD
-conx=pyodbc.connect('DSN={};UID={};PWD={}'.format(privConfig['CONN']['DNS'],
+#defino la conexion a la BD
+conx = pyodbc.connect('DSN={};UID={};PWD={}'.format(privConfig['CONN']['DNS'],
                                                   privConfig['CONN']['user'],
                                                   privConfig['CONN']['password']))
 
-#elijo fechas de corte para seleccion de datos y para train-test split
-spDay = date.today()- timedelta(days=90) #si uso tra test split no hace falta
+#elijo fechas de corte para seleccion de datos
+# spDay = date.today()- timedelta(days=90) #si uso tra test split no hace falta
 sinceDay = date.today() - timedelta(days=365*3) #entreno con X anios de historia
-# ACA DEBERIA HACER UN MAX DATE
+# ACA PODRIA HACER UN MAX DATE
 
 #%%
 
+#query de demanda sin acerias
 sqlDem=r"""
     SELECT TIME.FECHA DIA, TIME.HORA, TOP.RGE_NEMO REGION, SUM(DEM.DEMANDA_REAL_NETA) DEMANDA
     FROM SINGER.DEMANDA_HORARIA DEM
@@ -70,189 +73,174 @@ sqlDem=r"""
 
 qryDem = pd.read_sql(sqlDem,conx)
 
-
+# armo el index como timestamp, luego reemplazo dia y hora para reemplazar la H24 por 0
 qryDem.HORA = qryDem.HORA.astype('int')
 qryDem['Fecha'] = pd.to_datetime(qryDem.DIA) + pd.to_timedelta(qryDem.HORA, unit='h')
 qryDem.set_index('Fecha',inplace=True)
+qryDem.HORA = qryDem.index.hour
+qryDem.DIA = qryDem.index.date.astype('datetime64[ns]')
 
 
 #%%
+
 # Importo Feriados
 dfFer = pd.read_excel(pathFeriados,index_col=0)
 dfFer.drop(['Descripcion','Tipo'],inplace=True,axis=1) #el tipo de feriado se podría inplementar a futuro
-dfFer.set_index(pd.to_datetime(dfFer.index),inplace=True)
+dfFer.set_index(pd.to_datetime(dfFer.index).date,inplace=True)
+dfFer = dfFer.asfreq('D') 
+dfFer.index.names = ['DIA'] #renombro para join
+
+## rezagos de feriados
+dfFer['Feriado'].fillna(0,inplace=True)
+dfFer['L.Feriado'] = dfFer['Feriado'].shift(1)
+dfFer['F.Feriado'] = dfFer['Feriado'].shift(-1)
+dfFer['F.Feriado'].fillna(0,inplace=True) #el día posterior al último dato no se considera feriado
+dfFer['L.Feriado'].fillna(0,inplace=True) #el día anteriro al primer dato no se considera feriado
+
+# join con el df de feriados
+qryDem = qryDem.join(dfFer,on='DIA')
+
+
+#tratamiento del efecto de la cuarentena
+qryDem['COVID'] = np.zeros(len(qryDem.index))
+qryDem.loc['2020-03-20':'2020-05-05','COVID'] += 1 #Dias fuerte de cuarentena
+qryDem.loc['2020-05-06':'2020-05-25','COVID'] += 0.8 #despues se fue suavizando
+qryDem.loc['2020-05-26':'2020-11-01','COVID'] += 0.5 #se flexibiliza mas hasta normalidad
 
 
 #%%
 # ploteo para chequear
 # plt.plot(qryDem[(qryDem['REGION']=='GBA')].loc['01-01-2020':,'DEMANDA'])
 
-#%%
 
-region='GBA'
-
-estacion=estacionesMet[regionesEstaciones[region]].replace(" ","")
-
-appended_data = []
-
-pathEstacion = pathEstaciones + estacion + '*.csv'
-
-
-for i in glob.glob(pathEstacion, recursive=True):
-    dfEst = pd.read_csv(i,sep=',',index_col=0)
-    appended_data.append(dfEst)
-dfEst = pd.concat(appended_data)
-dfEst.set_index(pd.to_datetime(dfEst.index),inplace=True)
-
-
-dfDemanda = qryDem[(qryDem['REGION']==region)] #extraigo demanda para la region actual
-dfDemanda['dDEMANDA'] = dfDemanda['DEMANDA'].diff(1)
-dfDemanda.dropna(axis=0,inplace=True)
-
-dfDemanda = dfDemanda.join(dfEst,how='inner') #join con datos meteorologicos
-
-#aplico un filtro de suavizado de las var meteorologicas
-smVar=['TEMP','PNM','DD','FF','HUM']
-for i in smVar:
-    dfDemanda[i]=savgol_filter(dfDemanda[i].values,11,2)
-
-
-
-dfDemanda['TEMP2'] = dfDemanda['TEMP']**2
-dfDemanda['TEMP3'] = dfDemanda['TEMP']**3
-
-
-
-dfDemanda['MES'] = dfDemanda.index.month
-dfDemanda['TIPODIA'] = dfDemanda.index.dayofweek 
-
-dfDemanda = dfDemanda.join(dfFer)
-
-
-## FERIADOS
-dfDemanda['Feriado'].fillna(0,inplace=True)
-dfDemanda['L.Feriado']=dfDemanda['Feriado'].shift(24)
-dfDemanda['F.Feriado']=dfDemanda['Feriado'].shift(-24)
-dfDemanda['F.Feriado'].fillna(0,inplace=True) #el día posterior al último dato no se considera feriado
-dfDemanda['L.Feriado'].fillna(0,inplace=True) #el día anteriro al primer dato no se considera feriado
-
-# # VALORES REZAGADOS
-# shifted=[]
-
-# for i in range(1,4):
-#     dfDemanda['L{}.Dem'.format(i)]=dfDemanda['DEMANDA'].shift(i)
-#     shifted.append('L{}.Dem'.format(i))
-# dfDemanda.drop(dfDemanda.head(4).index, axis=0,inplace=True)
 
 #%%
 
-#Lista de features 
-featMeteo = ['TEMP','TEMP2','TEMP3','PNM','DD','FF','HUM']
-featCat = ['HORA', 'TIPODIA','MES']
-featFeriado = ['Feriado', 'L.Feriado','F.Feriado']
 
-feats = featMeteo + featCat + featFeriado #+ shifted
+for region in regionesElectricas:
 
+    #el nombre de la estacion para la region elegida:
+    estacion = estacionesMet[regionesEstaciones[region]].replace(" ","")
+    
+    #recursivamente abro los csv con los datos meteorológicos de esa estacion
+    appended_data = []
+    pathEstacion = pathEstaciones + estacion + '*.csv'
+    
+    for i in glob.glob(pathEstacion, recursive=True):
+        dfEst = pd.read_csv(i,sep=',',index_col=0)
+        appended_data.append(dfEst)
+    dfEst = pd.concat(appended_data)
+    dfEst.set_index(pd.to_datetime(dfEst.index),inplace=True)
+    
+    
+    dfDemanda = qryDem[(qryDem['REGION']==region)] #extraigo demanda para la region actual
 
-# DROPEO COLAPSO SI ESTA EN EL DATASET
-try:
-    dfDemanda.drop(dfDemanda.loc['2019-06-16',].index,axis=0,inplace=True) 
-except: 
-    pass
+    dfDemanda.dropna(axis=0,inplace=True)
+    
+    dfDemanda = dfDemanda.join(dfEst,how='inner') #join con datos meteorologicos
+    
+    #aplico un filtro de suavizado de las var meteorologicas
+    smVar = ['TEMP','PNM','DD','FF','HUM']
+    for i in smVar:
+        dfDemanda[i] = savgol_filter(dfDemanda[i].values,11,2)
+    
+    #potencia de las temperaturas
+    dfDemanda['TEMP2'] = dfDemanda['TEMP']**2
+    dfDemanda['TEMP3'] = dfDemanda['TEMP']**3
+    
+    dfDemanda['MES'] = dfDemanda.index.month
+    dfDemanda['TIPODIA'] = dfDemanda.index.dayofweek 
+    
 
-#Separacion TRAIN - TEST
-#Parte vieja
-# X_train = dfDemanda.loc[sinceDay:spDay,feats]
-# y_train = dfDemanda.loc[sinceDay:spDay,'DEMANDA']
-# X_test = dfDemanda.loc[spDay:,feats]
-# y_test = dfDemanda.loc[spDay:,'DEMANDA']
-
-X_train, X_test, y_train, y_test = train_test_split(dfDemanda.loc[since_day:,feats],
-                                                    dfDemanda.loc[since_day:,'DEMANDA'],
-                                                    test_size=0.15,random_state=42)
-
-
-#Instancio el modelo para el GridSearch
-model = XGBRegressor(booster ='gbtree',objective = 'reg:squarederror')
-
-param={
-    'learning_rate':[0.1],
-    'max_depth':[5,7],
-    'n_estimators':[400],
-    'colsample_bytree':[0.7,0.9],
-    'subsample':[0.7,0.9],
-    'gamma':[0], #min_split_loss
-    'reg_lambda':[30,50,100], #reg L2
-    'reg_alpha':[0,10] #reg L1
-}
-
-xgb_grid = GridSearchCV(model, param, cv=3 , verbose=False)
+    # DROPEO COLAPSO SI ESTA EN EL DATASET
+    try:
+        dfDemanda.drop(dfDemanda.loc['2019-06-16',].index,axis=0,inplace=True) 
+    except: 
+        pass
+    
+    # Train Test split
+    X_train, X_test, y_train, y_test = train_test_split(dfDemanda.loc[sinceDay:,feats],
+                                                        dfDemanda.loc[sinceDay:,'DEMANDA'],
+                                                        test_size=0.15,random_state=42)
+    
+    #ajusto el GridSearch SOLO para la primer región y luego uso los mismos hyperp
+    if region == regionesElectricas[0]:
+        #Instancio el modelo para el GridSearch
+        model = XGBRegressor(booster = 'gbtree',objective = 'reg:squarederror', n_jobs = 8)
+        
+        param={
+            'learning_rate':[0.1],
+            'max_depth':[5,7],
+            'n_estimators':[300],
+            'colsample_bytree':[0.7,0.9],
+            'subsample':[0.7,0.9],
+            'gamma':[0], #min_split_loss
+            'reg_lambda':[30,50], #reg L2
+            'reg_alpha':[0,10] #reg L1
+        }
+        
+        xgb_grid = GridSearchCV(model, param, cv=2)
+        
+        #AJUSTO LOS MEJORES PARAMETROS CON GRID SEARCH
+        xgb_grid.fit(X_train, y_train, 
+                eval_set = [(X_test, y_test)], 
+                early_stopping_rounds = 10, 
+                verbose=False)
+        
+        bestParams = xgb_grid.best_params_ #me guardo el dict
+        
+        print(xgb_grid.best_score_)
+        print(xgb_grid.best_params_)
+        #una vez que elegi los hyper, subo el n_estimator, y podría bajar el lr
+        bestParams['n_estimators'] = 1000
+        #bestParams['learning_rate'] = 0.01
+    
+    #fiteo el modelo definitivo
+    bestModel = XGBRegressor(booster ='gbtree',
+                            objective = 'reg:squarederror',
+                            n_jobs = -1,
+                            **bestParams)
+    
+    bestModel.fit(X_train, y_train, 
+                    eval_set = [(X_test, y_test)], 
+                    early_stopping_rounds = 20, 
+                    verbose=True)
+    
+    bestModel.save_model(r'json\{}_xgb.json'.format(region))
 
 #%%
 
-#AJUSTO LOS MEJORES PARAMETROS CON GRID SEARCH
-xgb_grid.fit(X_train, y_train, 
-        eval_set = [(X_test, y_test)], 
-        early_stopping_rounds = 10, 
-        verbose=True)
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.set(rc={'figure.figsize':(17, 8)})
 
-bestParams = xgb_grid.best_params_ #me guardo el dict
+# model = XGBRegressor() 
+# model.load_model('PAT_xgb.json')
 
-print(xgb_grid.best_score_)
-print(xgb_grid.best_params_)
+# fig,ax = plt.subplots(6,1,sharex=True)
 
+# start = '2021-08-10'
+# end = '2021-08-17'
+
+# idx = dfDemanda.loc[start:end,].index
+# yPred = model.predict(dfDemanda.loc[start:end,feats])
+# yTrue = dfDemanda.loc[start:end,'DEMANDA']
+
+# ax[0].plot(idx, yPred, linewidth=1, label='y_pred')
+# ax[0].plot(idx, yTrue,label='y_true')
+# ax[0].legend(loc='lower right',frameon=False)
+
+# ax[1].plot(dfDemanda.loc[start:end,'TEMP'])
+# ax[2].plot(dfDemanda.loc[start:end,'FF'])
+# ax[3].plot(dfDemanda.loc[start:end,'DD'])
+# ax[4].plot(dfDemanda.loc[start:end,'HUM'])
+# ax[5].plot(dfDemanda.loc[start:end,'PNM'])
 #%%
 
-bestParams['n_estimators']=1000
-bestParams['learning_rate']=0.01
 
-bestModel=XGBRegressor(booster ='gbtree',
-                        objective = 'reg:squarederror',
-                        **bestParams)
 
-bestModel.fit(X_train, y_train, 
-        eval_set = [(X_test, y_test)], 
-        early_stopping_rounds = 20, 
-        verbose=True)
-
-bestModel.save_model(region,'_xgb.json')
 
 #%%
-# 
-# fig,ax = plt.subplots()
-
-# start='2021-08-01 00'
-# end='2021-08-08'
-
-# idx = X_test.loc[start:end,].index
-# yPred = best_model.predict(X_test.loc[start:end,])
-# yTrue = y_test.loc[start:end,].values
-
-# ax.plot(idx, yPred, linewidth=1, label='y_pred')
-# ax.plot(idx,yTrue,label='y_test')
-
-# ax.legend(loc='lower right',frameon=False)
-
-#%%
-
-plt.scatter(X_test.loc[start:end,'MES'],(yTrue-yPred))
-
-#%%
-
-# dfDemanda.loc[start,'DEMANDA']
-
-# yPred2 = yPred
-
-# yPred2[0] = yPred[0] + dfDemanda.loc[y_train.index[-2],'DEMANDA']
-
-# for i in range(1,len(yPred)):
-#     yPred2[i]=yPred2[i]+yPred2[i-1]
-
-
-# plt.plot(idx, yPred2, linewidth=1, label='y_pred')
-# plt.plot(idx, dfDemanda.loc[start:end,'DEMANDA'],label='y_test')
-# plt.legend(loc='lower right',frameon=False)
-
 
 
 
